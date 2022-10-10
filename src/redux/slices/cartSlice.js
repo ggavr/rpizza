35dfb21f899ef3371940e06ce22d1bfe4fb94d74
ggavr <gimer.gavr@gmail.com>
2022-10-10 11:17:43 +0300

    ssd

--- src/redux/slices/cartSlice.js
+++ src/redux/slices/cartSlice.js
@@ -20,6 +20,12 @@ export const cartSlice = createSlice({
             state.totalPrice = state.items.reduce((sum,obj)=>{
             return (obj.price*obj.count)+sum},0)
     },
+        minusItem(state, action){
+            const findItem = state.items.find(obj => obj.id === action.payload)
+            if (findItem) {
+                findItem.count--
+            }
+        },
         removeItem (state, action) {
             state.items = state.items.filter(obj=>obj.id!==action.payload)
         },
@@ -29,6 +35,6 @@ export const cartSlice = createSlice({
     }
 })
 
-export const {addItem, removeItem, clearItem } = cartSlice.actions
+export const {addItem, removeItem, clearItem, minusItem } = cartSlice.actions
 
 export default cartSlice.reducer
